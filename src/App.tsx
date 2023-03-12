35f24c35b8f5663a7239b802a5b58e9135e8211a
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-12 15:25:06 +0200

    setup: get data from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Logo from "./assets/logo.svg";
 import Profile from "./assets/image-avatar.jpg";
 import "./sass/main.scss";
 import Toggle from "./components/Toggle";
+import QueryData from "./FetchData/QueryData";
 
 function App() {
   const [theme, setTheme] = useState(false);
@@ -11,25 +13,32 @@ function App() {
     setTheme(!theme);
   };
 
+  const queryClient = new QueryClient();
+
   return (
-    <div className="app">
-      <header className="header">
-        <div className="controls">
-          <div className="logo-container">
-            <a className="btn-logo" href="">
-              <img src={Logo} alt="" />
+    <QueryClientProvider client={queryClient}>
+      <div className="app">
+        <header className="header">
+          <div className="controls">
+            <div className="logo-container">
+              <a className="btn btn-logo" href="">
+                <img src={Logo} alt="" aria-hidden={true} />
+                <span className="sr-only">preprince investments</span>
+              </a>
+            </div>
+
+            <Toggle theme={theme} onChange={onChange} />
+          </div>
+          <div className="profile">
+            <a href="#" className="btn btn-profile">
+              <img src={Profile} alt="" aria-hidden={true} />
+              <span className="sr-only">customer profile</span>
             </a>
           </div>
-
-          <Toggle theme={theme} onChange={onChange} />
-        </div>
-        <div className="profile">
-          <a href="#" className="btn-profile">
-            <img src={Profile} alt="" />
-          </a>
-        </div>
-      </header>
-    </div>
+        </header>
+        <QueryData />
+      </div>
+    </QueryClientProvider>
   );
 }
 
