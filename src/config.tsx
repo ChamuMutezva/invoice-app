bd5937abe40575b32c0d8af0a90ab4f986712689
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-20 23:31:07 +0200

    trial cyclic deployment

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,4 @@
 export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://tasty-peplum-eel.cyclic.app/"
+    ? "https://tasty-peplum-eel.cyclic.app/api/invoices"
     : "http://localhost:4000/api/invoices"
     console.log(process.env.NODE_ENV);
