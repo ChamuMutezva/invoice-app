ec3844b2b6a68a7c1349df34ba54b99005e83a4d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-03-20 08:49:04 +0200

    format currency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,17 +20,32 @@ function HomePage() {
   if (isError) {
     return <h2>Error detected</h2>;
   }
+
+  // Create our number formatter.
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+
+    // These options are needed to round to whole numbers if that's what you want.
+    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+  });
+
   return (
     <div className="main">
       <h1 className="sr-only">Preprince investments business transactions</h1>
       <div className="flex summary">
         <div className="summary-headings">
           <h2 className="summary-title">Invoices</h2>
-          <p aria-live="polite">{invoices.length} invoices</p>
+          <p className="invoice-total-num" aria-live="polite">
+            {invoices.length} invoices
+          </p>
         </div>
         <div className="flex filter-new">
           <div className="select-options">
-            <label htmlFor="filter-options">Filter by</label>
+            <label className="filter-label" htmlFor="filter-options">
+              Filter
+            </label>
             <select
               name="choice"
               id="filter-options"
@@ -47,7 +62,9 @@ function HomePage() {
               <span className="container-img">
                 <img src={AddInvoiceImg} alt="" />
               </span>
-              New Invoice
+              <span className="flex btn-label">
+                New <span className="mobile-hidden">Invoice</span>
+              </span>
             </button>
           </Form>
         </div>
@@ -66,8 +83,8 @@ function HomePage() {
               <div key={invoice.id} className="card">
                 <p>{invoice.id}</p>
                 <p>{invoice.clientName}</p>
-                <p>{invoice.paymentDue}</p>
-                <p>R: {invoice.total}</p>
+                <p>Due {invoice.paymentDue}</p>
+                <p>{formatter.format(invoice.total)}</p>
                 <Link className="btn-link" to={`/viewInvoice/${invoice._id}`}>
                   {invoice.status}
                 </Link>
