8451fe15d9b4b4d36b1dac850f6f41c00b7010f5
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-04-07 14:19:14 +0200

    hook to add a new invoice

---
+++ src/hooks/createInvoice.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useMutation } from "react-query";
+import { API_ENDPOINT_PATH } from "../config";
+
+export default function createInvoice() {
+    const mutation= useMutation({
+        mutationFn: (newInvoice) => {
+            return axios.post(`${API_ENDPOINT_PATH}`, newInvoice)
+        }
+    })
+}
