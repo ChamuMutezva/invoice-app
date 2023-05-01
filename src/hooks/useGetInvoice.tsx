857eb3be16758ea2c2d456cc903b4063328c5492
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-05-01 08:45:26 +0200

    removed useGetInvoice hook usage replaced with useFetchSingleInvoice

--- src/hooks/useGetInvoice.tsx
+++
@@ -1,6 +0,0 @@
-/*import getInvoices from "./useGetInvoices";
-
-export default function getInvoice(id: any) {
-  const { invoices } = getInvoices("all");
-  return invoices?.find((invoice: { _id: any }) => invoice._id === id);
-} */
