264edeeb7e4767e17487a89c8deb6cf036202748
Chamunorwa Mutezva <ckmutezva@gmail.com>
2023-07-03 01:17:45 +0200

    login bypass

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 function LandingPage() {
 	return (
 		<div className="flex landing-page">
+			<p>authentication is down, use link to invoice page</p>
 			<Link to={"/signup"}>Sign up</Link>
-			<Link to={"/login"}>Login </Link>			
+			<Link to={"/login"}>Login </Link>
+			<Link to={`./invoicespage`}>Invoices page</Link>
 		</div>
 	);
 }
