cd2c34922421d5166c12bf9f68d16f41f443dd35
Chamu Mutezva <ckmutezva@gmail.com>
2024-06-28 20:02:53 +0200

    connecting to render - app deployed on render

--- src/config.tsx
+++ src/config.tsx
@@ -1,7 +1,12 @@
-export const API_ENDPOINT_PATH = process.env.NODE_ENV === 'production'
-    ? "https://tasty-peplum-eel.cyclic.app/api/invoices"
-    : "http://localhost:4000/api/invoices" 
+export const API_ENDPOINT_PATH =
+    process.env.NODE_ENV === "production"
+        ? "https://invoice-backend-jpep.onrender.com/api/invoices"
+        : "http://localhost:4000/api/invoices";
 
-export const API_AUTH_PATH = process.env.NODE_ENV === 'production'
-? "https://tasty-peplum-eel.cyclic.app/api/user"
-: "http://localhost:4000/api/user"
+export const API_AUTH_PATH =
+    process.env.NODE_ENV === "production"
+        ? "https://invoice-backend-jpep.onrender.com/api/user"
+        : "http://localhost:4000/api/user";
+
+// "https://tasty-peplum-eel.cyclic.app/api/invoices"
+// "https://tasty-peplum-eel.cyclic.app/api/user"
